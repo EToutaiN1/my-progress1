3d0971cbcd948ae30c80508ef6fdf1bd1767eba2
EToutaiN1 <toutain.ewan@gmail.com>
2022-07-09 18:08:15 +0200

    add Menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./css/app.css";
+import Menu from "./components/Menu";
 import Home from "./pages/Home";
 import TechnoAdd from "./pages/TechnoAdd";
-import TechnoList from "./pages/TechnoList"
+import TechnoList from "./pages/TechnoList";
+
 
 function App() {
   return (
     <>
+      <Menu />
       <Home />
       <TechnoAdd />
       <TechnoList />
