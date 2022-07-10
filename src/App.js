61eb3e003d8ff59f202f7daa232565aac630accd
EToutaiN1 <toutain.ewan@gmail.com>
2022-07-10 21:09:25 +0200

    End routage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
 import "./css/app.css";
 import Menu from "./components/Menu";
 import Home from "./pages/Home";
 import TechnoAdd from "./pages/TechnoAdd";
 import TechnoList from "./pages/TechnoList";
 
-
 function App() {
+  const [technos, setTechnos] = useState([]);
+
+  function handleAddTechno(techno) {
+    console.log("handleAddTechno", techno);
+  }
   return (
     <>
       <Menu />
-      <Home />
-      <TechnoAdd />
-      <TechnoList />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="add"
+          element={<TechnoAdd handleAddTechno={handleAddTechno} />}
+        />
+        <Route path="list" element={<TechnoList />} />
+      </Routes>
     </>
   );
 }
